bb71439b1bb1a937f1e5d8e80bf1f2032a9a4fb2
Guilherme Rocha <GuiPython2001@gmail.com>
2021-05-20 13:54:40 -0300

    Add Validation Vendedor com Annotations

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -8,18 +8,28 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="{0} required")]
+        [StringLength(60,MinimumLength =3,ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Invalid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
 
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")] 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [Range(0,50000,ErrorMessage = "{0} must be beetw")]
         [Display(Name = "Base Salary")]
         [DataType(DataType.Currency)]
         public double BaseSalary { get; set; }
