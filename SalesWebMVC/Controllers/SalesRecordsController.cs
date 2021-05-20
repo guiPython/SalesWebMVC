236ad2b6a0a401f6b419e719a84a9bc0b569db3a
Guilherme Rocha <GuiPython2001@gmail.com>
2021-05-20 17:46:50 -0300

    Add Vendas Service-Controller-Views com Relatorios

---
+++ SalesWebMVC/Controllers/SalesRecordsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Services;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        private readonly SalesRecordService _salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SimpleSearch(DateTime? init, DateTime? final)
+        {
+            if (!init.HasValue) init = new DateTime(2018, 9, 1);
+            if (!final.HasValue) final = new DateTime(2018, 10, 31);
+
+            ViewData["InitDate"] = init.Value.ToString("dd/MM/yyyy");
+            ViewData["FinalDate"] = final.Value.ToString("yyyy/MM/dd");
+
+            var sales = await _salesRecordService.FindByDateAsync(init, final);
+            return View(sales);
+        }
+
+        public async Task<IActionResult> GroupingSearch(DateTime? init, DateTime? final)
+        {
+            if (!init.HasValue) init = new DateTime(2018, 9, 1);
+            if (!final.HasValue) final = new DateTime(2018, 10, 31);
+
+            ViewData["InitDate"] = init.Value.ToString("yyyy/MM/dd");
+            ViewData["FinalDate"] = final.Value.ToString("yyyy/MM/dd");
+
+            var sales = await _salesRecordService.FindByDateGroupingAsync(init, final);
+            return View(sales);
+        }
+    }
+}
---
+++ SalesWebMVC/Services/SalesRecordService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMVCContext _dbContext;
+
+        public SalesRecordService(SalesWebMVCContext dbContext)
+        {
+            _dbContext = dbContext;       
+        }
+
+        public async Task<List<SaleRecord>> FindByDateAsync(DateTime? init, DateTime? final)
+        {
+            var result = from obj in _dbContext.SaleRecord select obj;
+
+            if (init.HasValue) result = result.Where(s => s.Date >= init.Value);
+
+            if (final.HasValue) result = result.Where(s => s.Date <= final.Value);
+
+            return await result.Include(s => s.Seller).Include(s => s.Seller.Department).OrderByDescending(s => s.Date).ToListAsync();
+        }
+
+        public async Task<List<IGrouping<Department,SaleRecord>>> FindByDateGroupingAsync(DateTime? init, DateTime? final)
+        {
+            var result = from obj in _dbContext.SaleRecord select obj;
+
+            if (init.HasValue) result = result.Where(s => s.Date >= init.Value);
+
+            if (final.HasValue) result = result.Where(s => s.Date <= final.Value);
+
+            var data = await result
+                .Include(s => s.Seller)
+                .Include(s => s.Seller.Department)
+                .OrderByDescending(s => s.Date)
+                .ToListAsync();
+
+            return data.GroupBy(s => s.Seller.Department).ToList();
+        }
+    }
+}
